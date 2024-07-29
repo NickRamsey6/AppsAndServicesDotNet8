54f5f1495f057dd82eaa2f95651c0c8166076965
NickRamsey6 <nramseysc@gmail.com>
2024-07-29 11:52:59 -0600

    Ch5 uncomment out code

--- Chapter05/WorkingWithTasks/Program.cs
+++ Chapter05/WorkingWithTasks/Program.cs
@@ -3,38 +3,38 @@
 OutputThreadInfo();
 Stopwatch timer = Stopwatch.StartNew();
 #region Running methods synchronously on one thread
-//SectionTitle("Running methods synchronously on one thread.");
-//MethodA();
-//MethodB();
-//MethodC();
+SectionTitle("Running methods synchronously on one thread.");
+MethodA();
+MethodB();
+MethodC();
 
 #endregion
 
 #region Running methods asynchronously on multiple threads
-//SectionTitle("Running methods asynchronously on multiple threads.");
+SectionTitle("Running methods asynchronously on multiple threads.");
 
-//Task taskA = new(MethodA);
-//taskA.Start();
-//Task taskB = Task.Factory.StartNew(MethodB);
-//Task taskC = Task.Run(MethodC);
+Task taskA = new(MethodA);
+taskA.Start();
+Task taskB = Task.Factory.StartNew(MethodB);
+Task taskC = Task.Run(MethodC);
 
-//Task[] tasks = { taskA, taskB, taskC };
-//Task.WaitAll(tasks);
+Task[] tasks = { taskA, taskB, taskC };
+Task.WaitAll(tasks);
 
 #endregion
 
 
 #region Passing the result of one task as an input into another
-//SectionTitle("Passing the result of one task as an input into another.");
+SectionTitle("Passing the result of one task as an input into another.");
 
-//Task<string> taskServiceThenSProc = Task.Factory
-//    .StartNew(CallWebService) // returns Task<decimal>
-//    .ContinueWith(previousTask => // returns Task<string>
-//        CallStoredProcedure(previousTask.Result));
+Task<string> taskServiceThenSProc = Task.Factory
+    .StartNew(CallWebService) // returns Task<decimal>
+    .ContinueWith(previousTask => // returns Task<string>
+        CallStoredProcedure(previousTask.Result));
 
-//WriteLine($"Result: {taskServiceThenSProc.Result}");
+WriteLine($"Result: {taskServiceThenSProc.Result}");
 
-//WriteLine($"{timer.ElapsedMilliseconds:#,##0}ms elapsed.");
+WriteLine($"{timer.ElapsedMilliseconds:#,##0}ms elapsed.");
 
 #endregion
 
