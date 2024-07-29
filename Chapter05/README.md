f24358a98d5607a3a25b4873a477d0b31ba71817
NickRamsey6 <nramseysc@gmail.com>
2024-07-29 11:17:23 -0600

    Ch5 Add Readme

---
+++ Chapter05/README.md
@@ -0,0 +1,14 @@
+# Chapter 5 Multitasking and Concurrency
+
+## Task Class Wait Methods
+| Method | Description |
+|--------|-------------|
+| t.Wait() | This waits for the task instance named t to complete execution. |
+| Task.WaitAny(Task[]) | This waits for any of the tasks in the array to complete execution. |
+| Task.WaitAll(Task[]) | This waits for all the tasks in the array to complete execution. |
+
+
+
+## Key Concepts
+
+## Practice Questions
--- Chapter05/WorkingWithTasks/Program.cs
+++ Chapter05/WorkingWithTasks/Program.cs
@@ -3,9 +3,16 @@
 OutputThreadInfo();
 Stopwatch timer = Stopwatch.StartNew();
 
-SectionTitle("Running methods synchronously on one thread.");
-MethodA();
-MethodB();
-MethodC();
+//SectionTitle("Running methods synchronously on one thread.");
+//MethodA();
+//MethodB();
+//MethodC();
+
+SectionTitle("Running methods asynchronously on multiple threads.");
+
+Task taskA = new(MethodA);
+taskA.Start();
+Task taskB = Task.Factory.StartNew(MethodB);
+Task taskC = Task.Run(MethodC);
 
 WriteLine($"{timer.ElapsedMilliseconds:#,##0}ms elapsed.");
